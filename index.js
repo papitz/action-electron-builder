18d94e95f099843d5e1bde22716d3b4a9e6ec5a9
Hunt Feng <spacecurvature@gmail.com>
2023-03-07 10:54:42 -0600

    change a line to use quasar build

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ const runAction = () => {
 	}
 
 	log(`Building${release ? " and releasing" : ""} the Electron app…`);
-	const cmd = useVueCli ? "vue-cli-service electron:build" : "electron-builder";
+	const cmd = useVueCli ? "vue-cli-service electron:build" : "quasar build -m electron";
 	for (let i = 0; i < maxAttempts; i += 1) {
 		try {
 			run(
