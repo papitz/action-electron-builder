8eab0d9067530392fc1281280183e7458fda2ab9
Hunt Feng <spacecurvature@gmail.com>
2023-08-03 02:31:49 -0600

    Update index.js

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ const runAction = () => {
 	}
 
 	log(`Building${release ? " and releasing" : ""} the Electron app…`);
-	const cmd = useVueCli ? "vue-cli-service electron:build" : "quasar build -m electron --debug";
+	const cmd = useVueCli ? "vue-cli-service electron:build" : "quasar build -m electron";
 	for (let i = 0; i < maxAttempts; i += 1) {
 		try {
 			run(
