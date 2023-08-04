09adb1d0cf1e550f215e7f4b4c9e887c0af21e0f
Paul <65065967+papitz@users.noreply.github.com>
2023-08-04 18:58:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -130,7 +130,7 @@ const runAction = () => {
 	for (let i = 0; i < maxAttempts; i += 1) {
 		try {
 			run(
-				"yarn build",
+				"yarn dist",
 				//`${useNpm ? "npx --no-install" : "yarn run"} ${cmd} --${platform} ${
 				//	release ? "--publish always" : ""
 				//} ${args}`,
